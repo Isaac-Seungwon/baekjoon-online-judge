e9bd328852bf936dde1a5531003b9d56ceb90c7a
Isaac-Seungwon <zhzkdkrak@naver.com>
2023-10-18 22:16:49 +0900

    [ADD] 11170 java solution

---
+++ B1_11170_0의개수/Main.java
@@ -0,0 +1,38 @@
+package B1_11170_0의개수;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int T = scan.nextInt(); // 테스트 케이스 수
+
+		for (int i = 0; i < T; i++) {
+			int N = scan.nextInt(); // 시작 숫자
+			int M = scan.nextInt(); // 끝 숫자
+			int count = 0;
+
+			for (int j = N; j <= M; j++) {
+				count += countZeros(j);
+			}
+
+			System.out.println(count);
+		}
+	}
+
+	public static int countZeros(int N) {
+		if (N == 0) {
+			return 1;
+		}
+		
+		int count = 0;
+		while (N > 0) {
+			if (N % 10 == 0) {
+				count++;
+			}
+			N /= 10;
+		}
+		return count;
+	}
+}
